c473ae86dc30376fd23be9efbfb5c1259b8fb96c
Param-10 <bheleparamveer@gmail.com>
2023-01-23 18:10:15 -0500

    On branch main  Your branch is up to date with 'origin/main'. Changes to be committed: 	modified:   #COP 2510 Section 2 Week 3 Lecture 1.py

--- #COP 2510 Section 2 Week 3 Lecture 1.py
+++ #COP 2510 Section 2 Week 3 Lecture 1.py
@@ -18,8 +18,40 @@ print(f'new number is {number}')
 number //=4
 print(f'new number is {number}')
 number%=10
-print(f'ne wnumber is {number}')
+print(f'new number is {number}')
 number**=3
 print(f'new number is {number}')
 
+#-----random module (and aliasing)-----
+import random as rn #rn - alias
+r=rn.random()
+print(f"Here's a random number between 0 and 1 (excel):{r:.7F}")
+r=rn.uniform(1,45)
+print(f"Here's a random number between 1 and 45:{r:.5f}")
+r=rn.randint(1,6)
+print(f"Here's a random number between 0 and 9:{r}")
+r=rn.randrange(10)
+print(f"Here's a random number between 0 and 9:{r}")
+r=rn.randrange(2,17)
+print(f"Here's a random number between 2 and 16:{r}")
+r=rn.randrange(10,51,5)
+print(f"Here's a random number between 10 and 50:{r}")
 
+#------selection--------
+#relational operators - <,>,<=,>=,==, !=
+#logical operators - not, and , or
+
+score = float(input('Enter a score:'))
+
+#one way selection
+if score>=80:
+    print('Yay! You advance, congrats')
+    print('Good job')
+
+#walrus operator version
+#two way selection
+if score:=float(input('Enter a score:'))>80:
+     print('Yay! You advance, congrats')
+     print('Good job')
+else:
+     print('oh no, try again')
