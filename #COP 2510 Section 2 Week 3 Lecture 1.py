5b429c4dc3697e4fad4b5cf9661103334b42e715
Param-10 <bheleparamveer@gmail.com>
2023-01-23 17:18:32 -0500

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	new file:   #COP 2510 Section 2 Week 3 Lecture 1.py

---
+++ #COP 2510 Section 2 Week 3 Lecture 1.py
@@ -0,0 +1,14 @@
+#COP 2510 Section 2 Week 3 Lecture 1
+#-----compound assignment statements-----
+number=18
+print(f'original number is {number}')
+ #number = number + 2
+number+=2
+print(f'new number is {number}')
+
+x=5
+y=6
+number *= x + y #20*(5+6)=20*11=220
+#number = number * x + y = 20*5+6=100+6
+print(f'new number is {number}')
+
