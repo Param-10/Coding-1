fa4492383bbbbade3e99cfe2ba349f8c5ee6c395
Param-10 <bheleparamveer@gmail.com>
2023-02-12 23:12:18 -0500

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: new file:   lab.py

---
+++ lab.py
@@ -0,0 +1,28 @@
+arrow_base_height = int(input('Enter arrow base height:\n'))
+
+arrow_base_width = int(input('Enter arrow base width:\n'))
+
+arrow_head_width = arrow_base_width
+
+while arrow_head_width <= arrow_base_width:
+
+   arrow_head_width = int(input('Enter arrow head width:\n'))
+
+print()
+
+for i in range(arrow_base_height):
+
+   for j in range(arrow_base_width):
+
+       print('*', end='')
+
+   print()
+
+for i in range(arrow_head_width):
+
+   for j in range(arrow_head_width-i):
+
+       print('*', end='')
+
+   print()
+
