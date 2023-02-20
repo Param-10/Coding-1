a752fd41885be16162c59ce0d775faa5be195fbc
Param-10 <bheleparamveer@gmail.com>
2023-02-20 18:11:15 -0500

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: modified:   Week7-Lec1.py 	new file:   minigw3.png

--- Week7-Lec1.py
+++ Week7-Lec1.py
@@ -1,5 +1,5 @@
 #COP 2510 Section 2 week 7 lecture 1
-
+from PIL import Image
 #------funcition definitions-----
 def main():
     print('Here are some popular games:')
@@ -13,6 +13,32 @@ def main():
 
     #call a function that uses a dictionary
     rename(animals)
+
+    #working with a image!
+    image1=Image.open("/Users/paramveer/Pictures/Lightroom Saved Photos/IMAGE1.jpg")
+
+    #display image(without using function)
+    image1.show()
+
+    #call image function
+    showpic(image1)
+
+    #create a thumbnail
+    image2=Image.open('/Users/paramveer/Pictures/Lightroom Saved Photos/image2.jpg')
+    image2.thumbnail((250,250))
+    image2.save("minigw3.png")
+    showpic(image2)
+
+    #test function that returns a list
+    value=int(input('what is the starting value?'))
+    amt=int(input('how many multiples?'))
+
+    valam=(value,amt)
+    #valam=
+
+
+
+    print(f'the first{amt} multiples of {value} are:\n{multiples(value,amt)}')
 
 
 
@@ -37,6 +63,7 @@ def esportgames():
         print(f'{rank}.{i}')
         rank-=1
     print()
+#------end esportgames-----------
 
 #void function that accepts a dictionary as a parameter
 def rename(ani):#ani to be a dictionary
@@ -44,6 +71,22 @@ def rename(ani):#ani to be a dictionary
         print(f'animal:{k}\t\t Alternate name:{ani[k]}')
 
 #-------end rename-----------
+
+#function that accepts an image
+def showpic(img):#img to be an image object
+    img.show()
+
+def multiples(v,a):
+    mult=[]
+    for i in range(1,a+1):
+        mult.append(i*v)#append - add values to the end of the list
+    return mult
+
+def multiples2(va):#va to be a tuple
+    mult=[]
+    for i in range(1,va):
+        mult.append(i*va[0])#append - add values to the end of the list
+    return mult
 
 
 #------no function definitions below this line---------
